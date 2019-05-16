10a0d6b4e241563e6a4ea27d19a845701c259b61
DGunn Razer Laptop <DGunn Razer Laptop@10.28.172.152>
2019-05-16 01:58:02 -0700

    CSS

---
+++ StartPage.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Darrien's Resume</title>
+        <link rel="stylesheet" type="text/css" media="all" href="css/Buttons.css">
+        <link rel="stylesheet" type="text/css" media="all" href="css/PageFormat.css">
+    </head>
+    <body>
+        <div class="body"/>
+        <form>
+            <a href="http://google.com" class="button orange bigrounded">Google</a>
+            <a href="https://www.linkedin.com/in/darrien-gunn-8a36a4185/" class="button orange bigrounded">linkedin</a>
+            <a href="https://github.com/dgunn001" class="button orange bigrounded">Github</a>
+        </form>
+    </body>
+    <?php 
+    #variable
+    echo '<p>Hello World</p>'; 
+    ?> 
+</html>
