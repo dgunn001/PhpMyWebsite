efbf478cba143d001e6fd76599415f8d5074edfc
DGunn Razer Laptop <DGunn Razer Laptop@10.28.172.152>
2019-05-16 07:35:19 -0700

    Added About Me and Formatting

--- StartPage.php
+++ StartPage.php
@@ -3,17 +3,32 @@
         <title>Darrien's Resume</title>
         <link rel="stylesheet" type="text/css" media="all" href="css/Buttons.css">
         <link rel="stylesheet" type="text/css" media="all" href="css/PageFormat.css">
+        <link rel="stylesheet" type="text/css" media="all" href="css/Text.css">
     </head>
-    <body>
-        <div class="body"/>
-        <form>
-            <a href="http://google.com" class="button orange bigrounded">Google</a>
-            <a href="https://www.linkedin.com/in/darrien-gunn-8a36a4185/" class="button orange bigrounded">linkedin</a>
-            <a href="https://github.com/dgunn001" class="button orange bigrounded">Github</a>
-        </form>
-    </body>
+    <div id="grad"/>
+        <body>
+            <div align="center">
+            <a id="Name"> <b>Darrien Gunn</b></a>
+            <div class="normaltext"> 
+                2nd Year, Computer Engineer
+            </div>
+            <form>
+                <div align="center">
+                <a href="http://google.com" class="button orange bigrounded"><b>Google</b></a>
+                <a href="https://www.linkedin.com/in/darrien-gunn-8a36a4185/" class="button orange bigrounded"><b>linkedin</b></a>
+                <a href="https://github.com/dgunn001" class="button orange bigrounded"><b>Github</b></a>
+                </div>
+            </form>
+            <a id="AbtTitle" > <b> About Me</b></a>
+            <div class="normaltext"> Hi! My name is Darrien Gunn and I was born and raised in San Francisco's Sunset District.
+                I am currently enrolled at UC Riverside. </br>
+                </br>
+                I'm also currently the social chair for Riverside Surging Dragons dragon boat team. When we do not have races I like to go to 
+                hackathons such as Biohack and CutieHack, which are all hosted at our school.
+            </div>
+        </body>
     <?php 
     #variable
-    echo '<p>Hello World</p>'; 
+ 
     ?> 
 </html>
